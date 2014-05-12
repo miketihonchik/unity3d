a0e399672af24de6961d9ef68f768aaeef70a0e8
miketihonchik <mike.tihonchik@gmail.com>
2014-05-12 11:41:43 -0500

    add simple 3rd person camera following

---
+++ FirstAttempt/Assets/ThirdPersonCamera.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+	[SerializeField]
+	private float distanceAway;
+	[SerializeField]
+	private float distanceUp;
+	[SerializeField]
+	private float smooth;
+	[SerializeField]
+	private Transform follow;
+	private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+		follow = GameObject.FindWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate () {
+		/* 
+		 * set target position to have correct offset from hovercraft 
+		 * so, we take current move up and backwards to get it correct
+		 */
+		targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+		Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
+        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+
+		/* making smooth transaction between current position and position it needs to be at */
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * smooth);
+		/* point camera in right direction */
+		transform.LookAt(follow);
+	}
+
+}
